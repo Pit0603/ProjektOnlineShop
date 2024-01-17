9fc65e2d4363874ee1753026cdc6b95377d53ee4
zumpe <zumpecorvin@gmail.com>
2024-01-17 17:49:04 +0100

    [Corvin] Impressum überarbeitet

--- html/Impressum.php
+++ html/Impressum.php
@@ -18,7 +18,7 @@
     <main>
                 <h1>Impressum</h1>
                 <p>
-                    <b>Seitenbetreiber i.S.d. § 5 TMG</b>
+                    <b tabindex = "0">Seitenbetreiber i.S.d. § 5 TMG</b>
                 </p><br>
                     <p>
                     Corvin Zumpe, Mohamed Widadi und Pit Dahlem<br>
@@ -28,19 +28,19 @@
                     Tel.: 0123 4567<br>
                     </p>
                 <p><br>
-                    <b>Inhaber:</b>
+                    <b tabindex = "0">Inhaber:</b>
                 </p><br>
                     <p>
                     Corvin Zumpe, Mohamed Widadi und Pit Dahlem
                     </p>
                 <p><br>
-                    <b>Umsatzsteuerindentifikations-Nr.:</b>
+                    <b tabindex = "0">Umsatzsteuerindentifikations-Nr.:</b>
                 </p><br>
                     <p>
                     DE123456789
                     </p>
                 <p><br>
-                    <b>Bankverbindung:</b>
+                    <b tabindex = "0">Bankverbindung:</b>
                 </p><br>
                     <p>
                     IBAN: DE123456789<br>
@@ -48,7 +48,7 @@
                     Bank: Bank
                     </p>
                 <p><br>
-                    <b>EU-Streitschlichtung</b>
+                    <b tabindex = "0">EU-Streitschlichtung</b>
                 </p><br>
                     <p>
                     Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:
@@ -56,28 +56,6 @@
                     Unsere E-Mail-Adresse finden Sie oben im Impressum.
                     </p>
         </main>
-            <footer class="footercontainer">
-                <hr>
-                <div>
-                <a href="https://de.facebook.com" target="_blank" title="Hier kommen sie zu unserer Facebook Seite">
-                <img src="../Icons/facebook-app-round-white-icon.svg" alt="Facebook"></a>
-                <a href="https://twitter.com" target="_blank" title="Hier kommen sie zu unserer X Seite">
-                <img id="X" src="../Icons/x-social-media-white-icon.svg" alt="X"></a>
-                <a href="https://instagram.com" target="_blank" title="Hier kommen sie zu unserer Instagram Seite">
-                <img src="../Icons/instagram-white-icon.svg" alt="Instagram"></a>
-            </div>
-            <h2>Rechtliches</h2>
-                <ul>
-                    <li><a href="Impressum.php" target="_self" title="Hier kommen Sie zu unserem Impressum">Impressum</a><br></li>
-                    <li><a href="Datenschutz.php" target="_self" title="Hier kommen Sie zu unserer Datenschutzerklärung">Datenschutz</a><br></li>
-                    <li><a href="Barrierefreiheit.php" target="_self" title="Hier kommen sie zur Erklärung zur Barrierefreiheit">Barrierefreiheit</a><br></li>
-                    <li><a href="Kontakt.php" target="_self" title="Hier kommen Sie zu unseren Kontaktinformationen">Kontakt</a><br></li>
-                </ul>
-                    <p>
-                    Corvin, Mohamed, Pit<br>
-                    <small> &copy; 2023 All Rights Reserved </small>
-                    </p>
-            </footer>
         </body>
     <?php
     include("footer.html");
