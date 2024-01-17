1f97bbf65225bf320ea65ef8f0289649c0211194
zumpe <zumpecorvin@gmail.com>
2024-01-17 17:54:48 +0100

    [Corvin] Barrierefreiheit text hinzugefügt

--- html/Barrierefreiheit.php
+++ html/Barrierefreiheit.php
@@ -20,9 +20,21 @@
                 <b>Erklärung zur Barrierefreiheit</b>
             </p><br>
                 <p>
-                Die Inhaber dieser Website bemühen sich, die Website <a href="index.php">Dragon Egg Emporium</a> barrierefreizugänglich zu machen.
-                Rechtsgrundlage sind das Landesgesetz zur Gleichstellung behinderter Menschen (LGGBehM) und die Barrierefreie-Informationstechnik-Verordnung Rheinland-Pfalz (BITV RP).
-                <br>
+                Die Inhaber dieser Website bemühen sich, die Website [Dragon Egg Emporium](index.php) barrierefreizugänglich zu machen. Rechtsgrundlage sind das Landesgesetz zur Gleichstellung behinderter Menschen (LGGBehM) und die Barrierefreie-Informationstechnik-Verordnung Rheinland-Pfalz (BITV RP). Um diese Zielsetzung zu erreichen, wurden folgende Maßnahmen zur Umsetzung der Barrierefreiheit ergriffen:
+
+                <br><br>1. **Navigieren durch Tabindex**: 
+                <br> Die Navigation auf der Website wurde so optimiert, dass Benutzer mit Hilfe der Tabulatortaste (Tab) durch alle interaktiven Elemente der Seite navigieren können. Dies erleichtert Personen mit motorischen Einschränkungen oder Nutzern, die keine Maus verwenden können, die Bedienung der Webseite.
+
+                <br><br>2. **Einfache Texte für besseres Verständnis**: 
+                <br>Die Inhalte der Website wurden in einfacher und klarer Sprache verfasst. Dies hilft nicht nur Menschen mit Lernschwierigkeiten oder kognitiven Beeinträchtigungen, sondern macht die Informationen auch für alle Nutzer leichter verständlich.
+
+                <br><br>3. **Link- und Bildtexte**: 
+                <br>Alle Links und Bilder auf der Website wurden mit beschreibenden Texten versehen. Bei den Links wird darauf geachtet, dass der Zweck des Links aus dem Text hervorgeht. Bilder werden durch alternative Texte (Alt-Texte) ergänzt, welche den Inhalt und Zweck der Bilder beschreiben. Diese Maßnahmen unterstützen Nutzer, die Screenreader verwenden, sowie jene, die aus verschiedenen Gründen keine Bilder sehen können oder wollen.
+
+                <br><br>4. **Screenreader-Kompatibilität**: 
+                <br>Die Website wurde für die Verwendung mit Screenreadern optimiert. Dies bedeutet, dass alle Inhalte und Navigationselemente so strukturiert sind, dass sie von Screenreadern korrekt erfasst und wiedergegeben werden können. Dadurch wird Menschen mit Sehbehinderungen der Zugang zu den Informationen der Website erleichtert.
+
+                <br>Durch diese Maßnahmen stellt die Website [Dragon Egg Emporium](index.php) sicher, dass sie für eine breite Palette von Benutzern, unabhängig von ihren körperlichen oder kognitiven Fähigkeiten, zugänglich und nutzbar ist.<br>
                 <br>
                 Diese Erklärung wurde am 11.11.2023 erstellt.
                 </p>
